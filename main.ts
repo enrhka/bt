f8855514f978a8d6b7b3ecdb9ac01093da4f7f98
enrhka <58929791+enrhka@users.noreply.github.com>
2023-03-12 16:15:09 +0900

    加入了變數的使用

--- main.ts
+++ main.ts
@@ -1,6 +1,24 @@
-input.onButtonPressed(Button.B, function () {
-    if (!(led.point(1, 1))) {
+input.onButtonPressed(Button.A, function () {
+    if (g == 0) {
+        g = 1
+    } else {
+        g = 0
+    }
+})
+let g = 0
+g = 0
+basic.forever(function () {
+    if (g == 1) {
         basic.showIcon(IconNames.Heart)
+        basic.pause(500)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+        basic.pause(500)
     } else {
         basic.clearScreen()
     }
